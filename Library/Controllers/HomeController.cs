98454cea1cc6a1124f7a06f6f8080a2068d1f61c
SmKou <66178856+SmKou@users.noreply.github.com>
2023-08-10 00:52:44 -0700

    WIP Add Home

---
+++ Library/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Library.Models;
+
+namespace Library.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly LibraryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public HomeController(LibraryContext db, UserManager<ApplicationUser> userManager)
+    {
+        _db = db;
+        _userManager = userManager;
+    }
+
+    public async Task<ActionResult> Index()
+    {
+        ViewBag.IsLoggedIn = User.Identity.IsAuthenticated;
+        if (ViewBag.IsLoggedIn)
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            ApplicationUser currentUser = await _userManager.FindByAsync(userId);
+            Person member = _db.Persons.SingleOrDefault(person => person.PersonId == currentUser.PersonId);
+            ViewBag.Name = member.FirstName;
+        }
+
+        if (TempData["Matches"] != null)
+            ViewBag.Matches = TempData["Matches"];
+        return View();
+    }
+
+    // Search for books by author and title
+    // public ActionResult Search(string query) { }
+}
